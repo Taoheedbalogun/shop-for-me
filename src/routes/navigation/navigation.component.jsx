2bd8e1883168b6524ad20dde03195ec6856c7df6
Taobal2 <baloguntaoheed2@gmail.com>
2024-09-24 10:04:06 +0100

    added navigation component

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,28 @@
+import { Outlet, Link } from "react-router-dom";
+import { Fragment } from "react";
+import { ReactComponent as CrownLogo } from "../../assets/crown.svg";
+import "./navigation.style.scss";
+import SignIn from "../sign-in/sign-in.component";
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <CrownLogo className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            Shop
+          </Link>
+          <Link className="nav-link" to="/signIn">
+            <SignIn />
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
